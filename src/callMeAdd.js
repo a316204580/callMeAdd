1b40d224cc1dfd57eebe7c0f9a9cc1879d7d0266
qwerty9307111 <2436415068@qq.com>
2018-03-06 11:58:00 +0800

    增加中间广告位；多个广告位显示优化

--- src/callMeAdd.js
+++ src/callMeAdd.js
@@ -1,5 +1,5 @@
 import './css/css.css';
-import $ from 'jquery';
+// import $ from 'jquery';
 import {_typeof} from "./checkDataTypes";
 
 class callMeAdd {
@@ -13,7 +13,7 @@ class callMeAdd {
      * interval 广告轮播时间间隔 默认3000ms
      * isShow 是否显示广告 默认false
      * className 广告控件class 默认无
-     * position 控件显示位置 默认 bottom-right  (top-left || top-right || bottom-left || bottom-right)
+     * position 控件显示位置 默认 bottom-right  (top-left || top-right || bottom-left || bottom-right || middle)
      * adds 广告信息数组 每一项 包含图片url及广告链接地址 必需
      */
     let w = o.width || 120, //width 宽度 默认120px·
@@ -25,22 +25,26 @@ class callMeAdd {
       display = o.isShow === void 0 ? false : o.isShow,//·
       c = o.className,  //className·
       adds = o.adds,
+      index = o.index || '',
       p = o.position || 'bottom-right'; //position·
 
-    let obj = {a, w, h, sc, ss, i, c, p, adds, display};
+    let obj = {a, w, h, sc, ss, i, c, p, adds, display, index};
 
     if (display && adds[0].src) {
       this.constructor.appendElement(obj);
-      this.constructor.setCloseAddEvents();
+      this.constructor.setCloseAddEvents(obj);
     }
   }
 
   static appendElement(e) {
-    let div = $(`<div class="call-me-add add-${e.p} ${e.c}" id="call-me-add" style="width: ${parseInt(e.w)}px;height: ${parseInt(e.h)}px;"><div class="close"><span id="close-add">关闭</span></div></div>`);
+    let w = e.w === '100%' ? e.w : e.w + 'px';
+    let h = e.h + 'px';
+
+    let div = $(`<div class="call-me-add add-${e.p} ${e.c}" id="call-me-add-${e.index}" style="width: ${w};height: ${h};"><div class="close"><span id="close-add-${e.index}">关闭</span></div></div>`);
 
     $('body').append(div);
 
-    let closeBtn = $('#close-add');
+    let closeBtn = $(`#close-add-${e.index}`);
     if (e.sc) {//显示关闭btn
       closeBtn.show();
     } else { //不显示关闭btn
@@ -58,8 +62,8 @@ class callMeAdd {
         this.appendSingleImg(e.adds[0], t)
       } else {//多张广告图片
         if (e.ss) {//开启轮播
-          let ul = $(`<ul class="add-img-container"></ul>`);
-          let ol = $(`<ul class="add-dot-container"></ul>`);
+          let ul = $(`<ul class="add-img-container add-img-container-${e.index}"></ul>`);
+          let ol = $(`<ul class="add-dot-container add-dot-container-${e.index}"></ul>`);
           for (let i = 0; i < e.adds.length; i++) {
             let imgLi = $(`<li data-index=${i}></li>`);
             let dotLi = $(`<li data-index=${i}></li>`);
@@ -77,7 +81,7 @@ class callMeAdd {
 
           t.append(ul).append(ol);
 
-          this.setSlideShow();
+          this.setSlideShow(e);
 
           if (e.a) {//开启自动播放
             this.setAutoPlay(e)
@@ -94,30 +98,32 @@ class callMeAdd {
 
   static setAutoPlay(o) {
     let t;
-    window.__index__ = 0;
+    let _index = '__index__' + o.p.replace('-', '_');
+    window[_index] = 0;
 
-    startInterval();
+    startInterval(o);
 
-    $('#call-me-add').on('mouseover', () => {
+    $(`#call-me-add-${o.index}`).on('mouseover', () => {
       stopInterval();
     }).on('mouseout', () => {
-      startInterval();
+      startInterval(o);
     });
 
-    function startInterval() {
-      let dotArr = $('.add-dot-container li');
-      let imgArr = $('.add-img-container li');
+    function startInterval(o) {
+      let dotArr = $(`.add-dot-container-${o.index} li`);
+      let imgArr = $(`.add-img-container-${o.index} li`);
 
       t = setInterval(() => {
 
-        window.__index__ = window.__index__ >= imgArr.length - 1 ? 0 : window.__index__ + 1;
+        window[_index] = window[_index] >= imgArr.length - 1 ? 0 : window[_index] + 1;
+        // console.log(_index + '=' + window[_index]);
 
         for (let x = 0, y = imgArr.length; x < y; x++) {
 
           $(dotArr[x]).removeAttr('id').removeClass('hover-dot');
-          $(dotArr[window.__index__]).attr('id', 'hover-dot').addClass('hover-dot');
+          $(dotArr[window[_index]]).attr('id', 'hover-dot').addClass('hover-dot');
 
-          if (x === window.__index__) {
+          if (x === window[_index]) {
             $(imgArr[x]).fadeIn('slow');
           } else {
             $(imgArr[x]).fadeOut('slow');
@@ -132,9 +138,9 @@ class callMeAdd {
 
   }
 
-  static setSlideShow() {
-    let dotArr = $('.add-dot-container li');
-    let imgArr = $('.add-img-container li');
+  static setSlideShow(o) {
+    let dotArr = $(`.add-dot-container-${o.index} li`);
+    let imgArr = $(`.add-img-container-${o.index} li`);
 
     for (let i = 0, j = dotArr.length; i < j; i++) {
 
@@ -177,12 +183,12 @@ class callMeAdd {
     t.append(img);
   }
 
-  static setCloseAddEvents() {
+  static setCloseAddEvents(o) {
     //关闭广告
-    $('#close-add').on('click', event => {
+    $(`#close-add-${o.index}`).on('click', event => {
       event.stopPropagation();
       event.preventDefault();
-      $('#call-me-add').hide();
+      $(`#call-me-add-${o.index}`).hide();
     });
   }
 
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const config = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
+    filename: '[name].min.js'
   },
   module: {
     rules: [
